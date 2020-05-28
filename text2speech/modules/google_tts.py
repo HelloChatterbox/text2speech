b477878091a8ec62fdae62b586cee9b95254ea22
jarbasal <jarbasai@mailfence.com>
2020-05-28 15:19:18 +0100

    cache default google langs

--- text2speech/modules/google_tts.py
+++ text2speech/modules/google_tts.py
@@ -24,6 +24,45 @@ logging.getLogger('gtts.tts').setLevel(logging.CRITICAL)
 class GoogleTTS(TTS):
     works_offline = False
     audio_ext = "mp3"
+    _default_langs = {'af': 'Afrikaans', 'sq': 'Albanian', 'ar': 'Arabic',
+                      'hy': 'Armenian', 'bn': 'Bengali', 'bs': 'Bosnian',
+                      'ca': 'Catalan', 'hr': 'Croatian', 'cs': 'Czech',
+                      'da': 'Danish', 'nl': 'Dutch', 'en': 'English',
+                      'eo': 'Esperanto', 'et': 'Estonian', 'tl': 'Filipino',
+                      'fi': 'Finnish', 'fr': 'French', 'de': 'German',
+                      'el': 'Greek', 'gu': 'Gujarati', 'hi': 'Hindi',
+                      'hu': 'Hungarian', 'is': 'Icelandic', 'id': 'Indonesian',
+                      'it': 'Italian', 'ja': 'Japanese', 'jw': 'Javanese',
+                      'kn': 'Kannada', 'km': 'Khmer', 'ko': 'Korean',
+                      'la': 'Latin', 'lv': 'Latvian', 'mk': 'Macedonian',
+                      'ml': 'Malayalam', 'mr': 'Marathi',
+                      'my': 'Myanmar (Burmese)', 'ne': 'Nepali',
+                      'no': 'Norwegian', 'pl': 'Polish', 'pt': 'Portuguese',
+                      'ro': 'Romanian', 'ru': 'Russian', 'sr': 'Serbian',
+                      'si': 'Sinhala', 'sk': 'Slovak', 'es': 'Spanish',
+                      'su': 'Sundanese', 'sw': 'Swahili', 'sv': 'Swedish',
+                      'ta': 'Tamil', 'te': 'Telugu', 'th': 'Thai',
+                      'tr': 'Turkish',
+                      'uk': 'Ukrainian', 'ur': 'Urdu', 'vi': 'Vietnamese',
+                      'cy': 'Welsh', 'zh-cn': 'Chinese (Mandarin/China)',
+                      'zh-tw': 'Chinese (Mandarin/Taiwan)',
+                      'en-us': 'English (US)', 'en-ca': 'English (Canada)',
+                      'en-uk': 'English (UK)', 'en-gb': 'English (UK)',
+                      'en-au': 'English (Australia)',
+                      'en-gh': 'English (Ghana)',
+                      'en-in': 'English (India)', 'en-ie': 'English (Ireland)',
+                      'en-nz': 'English (New Zealand)',
+                      'en-ng': 'English (Nigeria)',
+                      'en-ph': 'English (Philippines)',
+                      'en-za': 'English (South Africa)',
+                      'en-tz': 'English (Tanzania)',
+                      'fr-ca': 'French (Canada)',
+                      'fr-fr': 'French (France)',
+                      'pt-br': 'Portuguese (Brazil)',
+                      'pt-pt': 'Portuguese (Portugal)',
+                      'es-es': 'Spanish (Spain)',
+                      'es-us': 'Spanish (United States)'
+                      }
 
     def __init__(self, config=None):
         config = config or {"module": "google",
@@ -44,7 +83,10 @@ class GoogleTTS(TTS):
     def describe_voices(self):
         if self._voices is None:
             self._voices = {}
-            langs = tts_langs()
+            try:
+                langs = tts_langs()
+            except:
+                langs = self._default_langs
             for lang_code in langs:
                 self._voices[lang_code] = [langs[lang_code]]
         return self._voices
