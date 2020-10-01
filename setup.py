c87eb67d3ee2d18e8b58f101220764f7d92b3680
jarbasal <jarbasai@mailfence.com>
2020-10-01 22:12:14 +0100

    add license file

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='text2speech',
-    version='0.1.2',
+    version='0.1.3',
     packages=['text2speech', 'text2speech.modules'],
     url='https://github.com/HelloChatterbox/text2speech',
-    license='',
+    license='apache',
     author='jarbasAI',
     author_email='jarbasai@mailfence.com',
     description='TTS engines'
