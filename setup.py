3f71a8812fb9bb84c92358b5ddb042eba60ac224
jarbasal <jarbasai@mailfence.com>
2020-12-04 16:55:41 +0000

    .

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@ from setuptools import setup
 
 setup(
     name='text2speech',
-    version='0.1.3',
+    version='0.1.4',
     packages=['text2speech', 'text2speech.modules'],
     url='https://github.com/HelloChatterbox/text2speech',
     license='apache',
     author='jarbasAI',
+    install_requires=["ovos_utils"
+                      "requests_futures",
+                      "boto3",
+                      "ResponsiveVoice",
+                      "psutil",
+                      "gTTS>=2.2.1",
+                      "voxpopuli"],
     author_email='jarbasai@mailfence.com',
     description='TTS engines'
 )
